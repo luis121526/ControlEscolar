a83f54d080a2b59a5a3631d53306eb879e3bbdf2
luis121526 <diazdiaz121526@gmail.com>
2021-03-10 00:50:28 -0600

    f

---
+++ VistasUsuarios/Dashboard.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<link rel="stylesheet" type="text/css" href="../css/estilo.css">
+	<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+	<meta charset="utf-8">
+	<title>
+		Dashboard
+	</title>
+
+</head>
+<body>
+	<!--logo de la institucion-->
+<div class="logotipo">
+	<div class="itc">
+		<center><img src="../imagenes/logotipo.jpg" height="110" width="110"></center>
+	</div>
+	
+</div>
+
+	<!--logo de la institucion-->
+	<!--menu derecha de opciones alumno-->
+<div class="opciones">
+<div class="list-group">
+  <a href="../VistasUsuarios/Dashboard.php" class="list-group-item list-group-item-action">
+    Datos Generales
+  </a>
+  <a href="../VistasUsuarios/Documentos.php" class="list-group-item list-group-item-action">Carga De Documentos</a>
+  <a href="#" class="list-group-item list-group-item-action">Horario</a>
+  <a href="#" class="list-group-item list-group-item-action">Calificaciones</a>
+  <a href="#" class="list-group-item list-group-item-action">Kardex</a>
+  <a href="#" class="list-group-item list-group-item-action">Adeudos A Departamentos</a>
+  <a href="#" class="list-group-item list-group-item-action">Reinscripciones</a>
+  <a href="#" class="list-group-item list-group-item-action">Grupos Actuales</a>
+  <a href="#" class="list-group-item list-group-item-action">Grupos En Preparación</a>
+<a href="#" class="list-group-item list-group-item-action">Encuesta De Carga</a>
+<a href="#" class="list-group-item list-group-item-action">Pago De Servicios</a>
+<a href="#" class="list-group-item list-group-item-action">Cambio De Nip</a>
+<a href="#" class="btn btn-danger">Cerrar Sesion</a>
+<!--aqui terminan las opciones junto con el cierre de sesion-->
+</div>
+</div>
+<!--datos de usuario-->
+<div class="ip">
+	<img src="../imagenes/logo.png" height="50" width="80">
+</div>
+<div class="usuario">
+	<h6>15530378</h6>
+</div>
+<div class="cargo">
+	<h6>Estudiante</h6>
+</div>
+<!--mostrar datos-->
+<div class="DatosGen">
+	
+</div>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=0.86, maximum-scale=5.0, minimum-scale=0.86">
+	<title>inicio</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/estilo.css">
+
+
+</head>
+<body>
+	<div class="header">
+		<img src="./imagenes/logo2.png" >
+	</div>
+<div class="logo">
+<img src="./imagenes/logo.png" height="250" width="250" class="imagen">
+</div>
+<div class="login2">
+	<form>
+		<h6 class="center">Indroduzca Sus Datos De Acceso</h6>
+  <div class="mb-3">
+    <input type="text" class="form-control" name="nocontrol" placeholder="No.Control">
+  </div>
+  <div class="mb-3">
+    <input type="password" class="form-control" name="pass" placeholder="Contraseña">
+  </div>
+  <button type="submit" class="btn btn-primary">Accesar</button>
+</form>
+</div>
+</body>
+</html>
